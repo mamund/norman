009b560e9d874cd92eee1dccd408bbfb08528648
Mike Amundsen <mamund@yahoo.com>
2019-01-06 16:53:57 -0500

    add norman ref

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 /* 
- * norman utility
+ * name ....: norman utility
+ * date ....: 2019-01
+ * author ..: mamund
+ *
  * runs postman collections test via newman
  * see https://www.npmjs.com/package/newman#api-reference
- * 2019-01
- * mamund
+ *
+ * "Goodbye, Norman, Goodbye!"
+ * https://getyarn.io/yarn-clip/6e57047d-795a-4904-bdcf-784de3ed0066
  */
 
 "use strict";
